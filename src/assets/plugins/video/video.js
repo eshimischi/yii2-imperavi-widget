a19561e2918efa74a747089eb737b2e29d7189e5
eshimischi <eshimischi@gmail.com>
2019-11-12 11:06:32 +0300

    Модификация плагина Video

--- src/assets/plugins/video/video.js
+++ src/assets/plugins/video/video.js
@@ -1,75 +1,68 @@
-(function($)
-{
-	$.Redactor.prototype.video = function()
-	{
-		return {
-			reUrlYoutube: /https?:\/\/(?:[0-9A-Z-]+\.)?(?:youtu\.be\/|youtube\.com\S*[^\w\-\s])([\w\-]{11})(?=[^\w\-]|$)(?![?=&+%\w.-]*(?:['"][^<>]*>|<\/a>))[?=&+%\w.-]*/ig,
-			reUrlVimeo: /https?:\/\/(www\.)?vimeo.com\/(\d+)($|\/)/,
-			getTemplate: function()
-			{
-				return String()
-				+ '<section id="redactor-modal-video-insert">'
-					+ '<label>' + this.lang.get('video_html_code') + '</label>'
-					+ '<textarea id="redactor-insert-video-area" style="height: 160px;"></textarea>'
-				+ '</section>';
-			},
-			init: function()
-			{
-				var button = this.button.addAfter('image', 'video', this.lang.get('video'));
-				this.button.addCallback(button, this.video.show);
-			},
-			show: function()
-			{
-				this.modal.addTemplate('video', this.video.getTemplate());
+(function ($) {
+    $.Redactor.prototype.video = function () {
+        return {
+            reUrlYoutube: /https?:\/\/(?:[0-9A-Z-]+\.)?(?:youtu\.be\/|youtube\.com\S*[^\w\-\s])([\w\-]{11})(?=[^\w\-]|$)(?![?=&+%\w.-]*(?:['"][^<>]*>|<\/a>))[?=&+%\w.-]*/ig,
+            getTemplate: function () {
+                return String()
+                    + '<section id="redactor-modal-video-insert" class="redactor-tab redactor-tab2">'
+                    + '<label>Название видео:</label>'
+                    + '<input type="text" id="redaktor-video-title" aria-label="Название видео" />'
+                    + '<label>Youtube Url</label>'
+                    + '<textarea id="redactor-insert-video-area" style="height: 160px;" placeholder="Формат: https://www.youtube.com/watch?v=B5BVyY6glCQ"></textarea>'
+                    + '<div id="redactor-image-box"></div>'
+                    + '</section>';
+            },
+            init: function () {
+                if (!this.opts.imageManagerJson) 
+                    return;
+                
+                var button = this.button.addAfter('image', 'video', this.lang.get('video'));
+                this.button.addCallback(button, this.video.show);
+            },
+            show: function () {
+                var $modal = this.modal.getModal();
+            
+                this.modal.createTabber($modal);
+                this.modal.addTab(1, this.lang.get('upload'), 'active');
+                this.modal.addTab(2, 'Вставка видео');
 
-				this.modal.load('video', this.lang.get('video'), 700);
-				this.modal.createCancelButton();
+                $('#redactor-modal-image-droparea').addClass('redactor-tab redactor-tab1');
 
-				var button = this.modal.createActionButton(this.lang.get('insert'));
-				button.on('click', this.video.insert);
+                //var $box2 = $('<div id="redactor-image-manager-box" style="overflow: auto; height: 300px;" class="redactor-tab redactor-tab2">').hide();
+                //$modal.append($box2);
+                this.modal.addTemplate('video', this.video.getTemplate());
 
-				this.selection.save();
-				this.modal.show();
+                $.ajax({
+                    dataType: "json",
+                    cache: false,
+                    url: this.opts.imageManagerJson,
+                    success: $.proxy(function (data) {
+                        $.each(data, $.proxy(function (key, val) {
+                            // title
+                            var thumbtitle = '';
+                            if (typeof val.title !== 'undefined') thumbtitle = val.title;
+                            var id = '';
+                            if (typeof val.id !== 'undefined') id = val.id;
 
-				$('#redactor-insert-video-area').focus();
+                            this.modal.load('video', this.lang.get('video'), 700);
+                            this.modal.createCancelButton();
 
-			},
-			insert: function()
-			{
-				var data = $('#redactor-insert-video-area').val();
+                            var button = this.modal.createActionButton(this.lang.get('insert'));
+                            button.on('click', this.video.insert);
 
-				if (!data.match(/<iframe|<video/gi))
-				{
-					data = this.clean.stripTags(data);
+                            var img = $('<img src="' + val.thumb + '" rel="' + val.image + '" title="' + thumbtitle + '" data-id="' + id + '" style="width: 100px; height: 75px; cursor: pointer;" />');
+                            $('#redactor-image-box').append(img);
 
-					// parse if it is link on youtube & vimeo
-					var iframeStart = '<iframe style="width: 500px; height: 281px;" src="',
-						iframeEnd = '" frameborder="0" allowfullscreen></iframe>';
+                            this.selection.save();
+                            this.modal.show();
 
-					if (data.match(this.video.reUrlYoutube))
-					{
-						data = data.replace(this.video.reUrlYoutube, iframeStart + '//www.youtube.com/embed/$1' + iframeEnd);
-					}
-					else if (data.match(this.video.reUrlVimeo))
-					{
-						data = data.replace(this.video.reUrlVimeo, iframeStart + '//player.vimeo.com/video/$2' + iframeEnd);
-					}
-				}
+                            $('#redactor-insert-video-area').focus();
 
-				this.selection.restore();
-				this.modal.close();
-
-				var current = this.selection.getBlock() || this.selection.getCurrent();
-
-				if (current) $(current).after(data);
-				else
-				{
-					this.insert.html(data);
-				}
-
-				this.code.sync();
-			}
-
-		};
-	};
+                        }, this));
+                    }, this)
+                });
+            },
+            insert: function () {}
+        };
+    };
 })(jQuery);
